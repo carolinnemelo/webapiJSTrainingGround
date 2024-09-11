0ea03a36d43142452f22bd693845b1913dd3172a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-11 13:29:51 +0200

    create helper function that returns filename

--- index.ts
+++ index.ts
@@ -1,21 +1,30 @@
-// console.log("b")
-
 import http, { IncomingMessage, ServerResponse } from "http";
-import fs from 'fs';
+import fs from "fs";
 
+const fileNameOfUrl = (url: string) => {
+	let fileName = "";
+	if (url.split("/")[1] === "") {
+		fileName = "index.html";
+	} else {
+		fileName = url.split("/")[1];
+	}
+	return fileName;
+};
 
-
-
-const server = http.createServer( //handler or request handling function
+const server = http.createServer(
+	//handler or request handling function
 	(req: IncomingMessage, res: ServerResponse) => {
-        const content = fs.readFileSync('./static/index.html', 'utf-8'); 
-        
+		console.log(`The URL for the request was '${req.url}'`);
+		console.log(`The METHOD for the request was '${req.method}'`);
+
+        const fileName = fileNameOfUrl(req.url);
+		const content = fs.readFileSync("./static/index.html", "utf-8");
+
 		res.statusCode = 200;
 		res.setHeader("Content-Type", "text/html");
 		res.end(content);
 	}
 );
-
 
 const hostname: string = "localhost";
 const port: number = 3000;

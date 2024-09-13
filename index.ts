22f3d2a23afee6e027dedf7733defbbf7b55c418
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-13 16:21:28 +0200

    create the app.delete

--- index.ts
+++ index.ts
@@ -16,18 +16,45 @@ const db = [
 	},
 ];
 
+app.use(express.json());
+
 app.get("/api/developers", (req: Request, res: Response) => {
-	res
-    .status(200)
-    .json(db);
+	res.status(200).json(db);
 });
 
 app.get("/api/developers/:id/:name/:address", (req, res) => {
-    const id = Number(req.params.id);
-    const devById = db.find(e => e.id === id);
+	const id = Number(req.params.id);
+	const devById = db.find((e) => e.id === id);
 
-    return devById ? res.status(404).end() : res.json(devById);
+	return devById ? res.status(404).end() : res.json(devById);
+});
 
+app.post("/api/developers/", (req, res) => {
+	const newDev = {
+		id: db.length + 1,
+		name: req.body.name,
+		email: req.body.email,
+	};
+
+	db.push(newDev);
+
+	res
+		.status(201)
+		.setHeader("location", `/api/developers/${newDev.id}`)
+		.json(newDev);
+});
+
+app.delete("/api/developers/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const devById = db.find((e) => e.id === id);
+    
+	if (devById) {
+		db.splice(db.indexOf(devById), 1);
+
+		res.status(204).end();
+	} else {
+		res.status(404).end();
+	}
 });
 
 const port = 3000;

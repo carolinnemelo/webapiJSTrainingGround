2d92a4c9fd0965bb3686bc80d62ea9657b1754c2
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-16 13:11:44 +0200

    add return and delete elses

--- index.ts
+++ index.ts
@@ -40,67 +40,69 @@ app.post("/api/developers/", (req, res) => {
 
 	if (!name) {
 		res.status(400).send("Name is required");
-        return;
-	} else if (!email) {
-		res.status(400).send("Email is required");
-        return;
-	} else if (db.find((dev) => dev.name === name || dev.email === email)) {
-		res
-        .status(409)//Conflict
-        .send(`Developer with the same name or email already exists`);
-        
-	} else {
-		const newDev: Developer = {
-			id: db.length + 1,
-			name: req.body.name,
-			email: req.body.email,
-		};
-
-		db.push(newDev);
-
-		res
-			.status(201)
-			.setHeader("location", `/api/developers/${newDev.id}`)
-			.json(newDev);
+		return;
 	}
-});
 
+	if (!email) {
+		res.status(400).send("Email is required");
+		return;
+	}
+
+	if (db.find((dev) => dev.name === name || dev.email === email)) {
+		res
+			.status(409) //Conflict
+			.send(`Developer with the same name or email already exists`);
+		return;
+	}
+
+	const newDev: Developer = {
+		id: db.length + 1,
+		name: req.body.name,
+		email: req.body.email,
+	};
+
+	db.push(newDev);
+
+	res
+		.status(201)
+		.setHeader("location", `/api/developers/${newDev.id}`)
+		.json(newDev);
+});
 
 app.patch("/api/developers/:id", (req, res) => {
 	const id = Number(req.params.id);
 	const devById = db.find((e) => e.id === id);
 	if (!devById) {
 		res.status(404).end();
+		return;
+	}
+	if (req.body.name) {
+		devById.name = req.body.name;
+	}
 
-	} else {
-        if (req.body.name){
-            devById.name = req.body.name;
-        }
+	if (req.body.email) {
+		devById.email = req.body.email;
+	}
 
-        if(req.body.email) {
-            devById.email = req.body.email;
-        }
+	console.log(devById);
 
-        console.log(devById)
-        
-        res
-            .status(201)
-            .setHeader("location", `/api/developers/${devById.id}`)
-            .json(devById);
-    }
+	res
+		.status(201)
+		.setHeader("location", `/api/developers/${devById.id}`)
+		.json(devById);
 });
 
 app.delete("/api/developers/:id", (req, res) => {
 	const id = Number(req.params.id);
 	const devById = db.find((e) => e.id === id);
-    
+
 	if (devById) {
 		db.splice(db.indexOf(devById), 1);
-
 		res.status(204).end();
-	} else {
-		res.status(404).end();
+		return;
 	}
+
+	res.status(404).end();
 });
 
 const port = 3000;

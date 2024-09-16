e79048a6eab81cecb3ec4805e7dd741c70e2585d
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-16 15:33:15 +0200

    add assert inside expect

--- test.ts
+++ test.ts
@@ -2,14 +2,20 @@ import assert from "assert";
 import request from "supertest";
 import app from "./api";
 
+
 describe("developer API should have endpoints to", () => {
 	it("get all developers", (done) => {
 		//arrange
 		//act and assert
 		request(app)
 			.get("/api/developers")
-            .set("Accept", "application/json")
-            .expect("Content-Type", /json/)
-            .expect(200, done);
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.expect((res) => {
+				assert.strictEqual(res.body.length, 2);
+			})
+			.expect(200, done);
+
 	});
 });
+ 
